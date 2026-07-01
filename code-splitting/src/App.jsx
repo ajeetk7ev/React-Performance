02ccfc716d30eec5001675f7faa5a166c762f752
ajeetk7ev <ajeetk8568@gmail.com>
2026-07-01 09:15:27 +0530

    Add the suspance

--- code-splitting/src/App.jsx
+++ code-splitting/src/App.jsx
@@ -1,29 +1,30 @@
-import React from "react";
-import { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-const Profile = lazy(() => import('./pages/Profile'));
-const Contact = lazy(() => import('./pages/Contact'));
-const About = lazy(() => import('./pages/About'));
-const FAQ = lazy(() => import('./pages/FAQ'));
-const Login = lazy(() => import('./pages/Login'));
+
+const Profile = lazy(() => import("./pages/Profile"));
+const Contact = lazy(() => import("./pages/Contact"));
+const About = lazy(() => import("./pages/About"));
+const FAQ = lazy(() => import("./pages/FAQ"));
+const Login = lazy(() => import("./pages/Login"));
+
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   <div>
-      <Navbar/>
-     <Routes>
-      <Route path="/" element={<h1>Home page</h1>} />
-      <Route path="profile" element={<Profile />} />
-      <Route path="contact" element={<Contact />} />
+    <div>
+      <Navbar />
 
-      <Route path="about" element={<About />} />
-
-      <Route path="faqs" element={<FAQ />} />
-
-      <Route path="login" element={<Login/>} />
-    </Routes>
-   </div>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="faqs" element={<FAQ />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </div>
   );
 }
 
